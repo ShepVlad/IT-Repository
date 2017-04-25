38035538c16b4c994cdd1c6e6d132aef3a3fa525
Natasha <Nidialkova@ukr.net>
2017-04-25 21:51:27 +0300

    Change authoriz

--- IT_Repository/Autho.aspx.cs
+++ IT_Repository/Autho.aspx.cs
@@ -4,10 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Xml.Linq;
 
 public partial class Autho : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        string email = Request.Form["email"];
+        string password = Request.Form["password"];
+       
+
+        string path = Server.MapPath("Users.xml");
+        XDocument doc = XDocument.Load(path);
+
+        var res = from u in doc.Element("users").Elements("user")
+                  where u.Attribute("email").Value == email &&
+                  u.Attribute("password").Value == password
+                  select u;
+        if(res.Count() == 0)
+        {
+            //false;
+        }
+        else
+        {
+
+            //true;
+        }
+    }
+
+    protected void btnForgot_Click(object sender, EventArgs e)
     {
 
     }
