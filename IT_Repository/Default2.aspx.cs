2ce5073a692c34e10493a9b4485b5aca2a3c8a3b
teacher <vpopov@itstep.org>
2017-04-25 21:17:11 +0300

    Дополнение к примеру авторизации по data.xml

--- IT_Repository/Default2.aspx.cs
+++ IT_Repository/Default2.aspx.cs
@@ -18,13 +18,34 @@ public partial class Default2 : System.Web.UI.Page
         string user = Request.Form["user"];
         string age = Request.Form["age"];
         output.Text = "user: " + user + "; age: " + age;
-
+        
         string path = Server.MapPath("data.xml");
         XDocument doc = XDocument.Load(path);
+
+        var res = from x in doc.Element("data").Elements("person")
+                  where x.Attribute("user").Value == user &&
+                        x.Attribute("age").Value == age
+                  select x;
+
+        if (res.Count() == 0)
+        {
+            output.Text = "FAILED";
+        }
+        else
+        {
+            output.Text = "OKAY";
+        }
+
+        //  Проверка пользователя с аккаунтом [user/age]:
+        // ==============================================
+
+
+        /*
         doc.Element("data").Add(
             new XElement("person",
                 new XAttribute("user", user),
                 new XAttribute("age", age)));
         doc.Save(path);
+        */
     }
 }
