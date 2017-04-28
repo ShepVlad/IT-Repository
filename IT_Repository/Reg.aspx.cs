a7e6ac274e8e2ed706002c8cd55af20f5e71211c
ShepVlad <vladik_shepeliuk@mail.ru>
2017-04-28 22:55:32 +0300

    Доделал регистрацию но проблема со страной осталась

--- IT_Repository/Reg.aspx.cs
+++ IT_Repository/Reg.aspx.cs
@@ -32,7 +32,7 @@ public partial class Reg : System.Web.UI.Page
         string lname = txtLastName.Text;
         string pass = txtPassword.Text;
         string email = txtEmail.Text;
-        string country = ddlCountry.SelectedItem.Text;
+        string country = ddlCountry.SelectedItem.ToString();
 
 
         //string path = Server.MapPath("Users.xml");
@@ -54,17 +54,32 @@ public partial class Reg : System.Web.UI.Page
         //        ));
         //doc.Save(path);
 
-        int id = 2;
+         int id ;
+        string idshka="";
         string connectionstring = "Data Source=SQL6001.SmarterASP.NET;Initial Catalog=DB_A1F08F_ITPortalRep;User Id=DB_A1F08F_ITPortalRep_admin;Password=VladVsemRad123456;";
         SqlConnection conn = new SqlConnection(connectionstring);
         conn.Open();
-        string query = "Insert into Users(id,name,lname,email,country,role,password) values ({0},'{1}','{2}','{3}','{4}','{5}','{6}')";
-        query = string.Format(query, id, name, lname, email, country, "user", pass);
+
+      //  string query = "SELECT * FROM dbo.Users)";
+        SqlDataAdapter dataadapter = new SqlDataAdapter("SELECT * FROM dbo.Users",conn);
+        DataTable users = new DataTable();
+        dataadapter.Fill(users);
+        foreach (DataRow item in users.Select())
+        {
+            idshka = item.ItemArray[0].ToString();
+        }
+        id = Convert.ToInt32(idshka);
+        id += 1;
+
+
+        string query = string.Format("Insert into Users(id,name,lname,email,country,role,password) values ({0},'{1}','{2}','{3}','{4}','{5}','{6}')"
+            ,id,name,lname,email,country,"user",pass);
+        
         SqlCommand cmd = new SqlCommand(query, conn);
         cmd.ExecuteNonQuery();
         conn.Close();
         Response.Redirect("Autho.aspx");
 
-        //  Response.Redirect("");
+
     }
 }
